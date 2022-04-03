936b3bcb2507bfbb3c61d3f4b5839440df7fa34c
jaikumarhero <102908387+jaikumarhero@users.noreply.github.com>
2022-04-03 18:45:16 +0500

    Create *setup.sh

---
+++ *setup.sh
@@ -0,0 +1,38 @@
+#Facebook : Jaikumar Parhyar
+
+#Github   : https://github.com/jaikumarhero/jaikumarhero
+
+#Whatsapp : +92312734753
+
+clear
+
+echo
+
+echo "Install All You Need, Patient Bro ..!"
+
+echo
+
+pkg update && pkg upgrade
+
+apt update
+
+apt upgrade
+
+pkg install python
+
+pkg install python2
+
+pip2 install requests
+
+pip2 install mechanize
+
+pip2 install bs4
+
+pip2 install lolcat
+
+pkg install nodejs-lts
+
+pip2 install requests
+
+pkg install nodejs
+
